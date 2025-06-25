7f4b6519d64e1322476a2d70c273265ccb8aeb87
Lotchi Serge Gogo <serge.gogolo12@gmail.com>
2025-06-25 00:07:58 -0300

    add json response function

--- api/authorController.go
+++ api/authorController.go
@@ -18,19 +18,7 @@ import (
 var ErrEmptyAuthorRequestBody error = errors.New("please inform author body")
 
 func StoreAuthor(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 	services.Write(r.Method + " " + r.URL.Path)
-	params := r.Body
-	var response services.HttpResponse
-
-	if params == nil {
-		w.WriteHeader(http.StatusNoContent)
-		response.Code = http.StatusNoContent
-		response.Message = ErrEmptyAuthorRequestBody.Error()
-		json.NewEncoder(w).Encode(response)
-		return
-	}
-
 	var author author.Author
 	db.ConnSqlite()
 	json.NewDecoder(r.Body).Decode(&author)
@@ -38,158 +26,92 @@ func StoreAuthor(w http.ResponseWriter, r *http.Request) {
 	author.UpdatedAt = nil
 	id, err := author.Persist(db.Connection)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		response.Code = http.StatusBadRequest
-		response.Message = err.Error()
-		json.NewEncoder(w).Encode(response)
+		msg := err.Error()
+		services.JSONError(w, msg, nil, http.StatusBadRequest)
 		return
 	}
 	author.Id = *id
-	dto := dto.AuthorToResource(author)
-	tobyte, err := json.MarshalIndent(&dto, "", "\t")
+	data := dto.AuthorToResource(author)
 
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		response.Code = http.StatusBadRequest
-		response.Message = ErrEmptyAuthorRequestBody.Error()
-		json.NewEncoder(w).Encode(response)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	response.Code = http.StatusCreated
-	response.Message = "author created successfully"
-	response.Data = (*json.RawMessage)(&tobyte)
-	json.NewEncoder(w).Encode(response)
+	message := "author created successfully"
+	services.JSONSuccess(w, message, data, http.StatusCreated)
 }
 
 func Show(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 	services.Write(r.Method + " " + r.URL.Path)
-	var response services.HttpResponse
 	params := mux.Vars(r)
 	db.ConnSqlite()
-	parseId, err := strconv.ParseInt(params["id"], 0, 32)
+	id, err := strconv.Atoi(params["id"])
 	if err != nil {
-		panic(err)
+		er := err.Error()
+		services.JSONError(w, er, nil, http.StatusBadRequest)
+		return
 	}
-	id := int(parseId)
 	author, err := author.Find(db.Connection, id)
 	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		response.Code = http.StatusNotFound
-		response.Message = err.Error()
-		json.NewEncoder(w).Encode(response)
+		er := err.Error()
+		services.JSONError(w, er, nil, http.StatusNotFound)
 		return
 	}
 	authorDto := dto.AuthorToResource(*author)
-	data, err := json.MarshalIndent(authorDto, "", "\t")
-	if err != nil {
-		panic(err)
-	}
-	response.Message = "Showing an author"
-	response.Code = http.StatusOK
-	response.Data = (*json.RawMessage)(&data)
-	json.NewEncoder(w).Encode(response)
+	message := "Showing an author"
+	services.JSONSuccess(w, message, authorDto, http.StatusOK)
 }
 
 func ListAll(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var response services.HttpResponse
 	services.Write(r.Method + " " + r.URL.Path)
 	db.ConnSqlite()
 	authors, err := author.FindAll(db.Connection)
-
 	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		response.Code = http.StatusNotFound
-		response.Message = err.Error()
-		json.NewEncoder(w).Encode(response)
+		msg := err.Error()
+		services.JSONSuccess(w, msg, nil, 404)
 		return
 	}
 	collection := dto.AuthorsCollection(*authors)
-	data, err := json.MarshalIndent(collection, "", "\t")
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		response.Code = http.StatusNotFound
-		response.Message = err.Error()
-		json.NewEncoder(w).Encode(response)
-		return
-	}
-	response.Code = http.StatusOK
-	response.Message = "List of all author"
-	response.Data = (*json.RawMessage)(&data)
-	json.NewEncoder(w).Encode(response)
+	message := "List of all author"
+	services.JSONSuccess(w, message, collection, 200)
 
 }
 
 func UpdateAuthor(w http.ResponseWriter, r *http.Request) {
 	services.Write(r.Method + " " + r.URL.Path)
-	w.Header().Set("Content-Type", "application/json")
-	var response services.HttpResponse
 	var author author.Author
-
-	params := r.Body
-	if params == nil {
-		w.WriteHeader(http.StatusNoContent)
-		response.Code = http.StatusNoContent
-		response.Message = "Please informe a author data"
-		json.NewEncoder(w).Encode(response)
-		return
-	}
 	db.ConnSqlite()
 	json.NewDecoder(r.Body).Decode(&author)
 	result, err := author.Update(db.Connection)
 	if err != nil {
-		w.WriteHeader(http.StatusConflict)
-		response.Code = http.StatusConflict
-		response.Message = err.Error()
-		json.NewEncoder(w).Encode(response)
+		message := err.Error()
+		services.JSONError(w, message, nil, 501)
 		return
 	}
 	dto := dto.AuthorToResource(*result)
-	data, _ := json.MarshalIndent(dto, "", "\t")
-	w.WriteHeader(http.StatusOK)
-	response.Code = http.StatusOK
-	response.Message = "Author updated successfully"
-	response.Data = (*json.RawMessage)(&data)
-
-	json.NewEncoder(w).Encode(response)
+	message := "Author updated successfully"
+	services.JSONSuccess(w, message, dto, 200)
 }
 
 func DeleteAuthor(w http.ResponseWriter, r *http.Request) {
 	services.Write(r.Method + " " + r.URL.Path)
-	w.Header().Set("Content-Type", "application/json")
-	var response services.HttpResponse
 	params := mux.Vars(r)
-	parseId, err := strconv.ParseInt(params["id"], 0, 64)
+	parseId, err := strconv.Atoi(params["id"])
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		response.Message = fmt.Sprintf("Parametro %s Invalido: %v", params["id"], err.Error())
-		response.Code = http.StatusBadRequest
-		json.NewEncoder(w).Encode(response)
+		message := fmt.Sprintf("Parametro %s Invalido: %v", params["id"], err.Error())
+		services.JSONError(w, message, nil, http.StatusBadRequest)
 		return
 	}
 	db.ConnSqlite()
 	id := int(parseId)
 	author, err := author.Find(db.Connection, id)
 	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		response.Message = fmt.Sprintf("Erro : %s", err.Error())
-		response.Code = http.StatusBadRequest
-		json.NewEncoder(w).Encode(response)
+		message := err.Error()
+		services.JSONError(w, message, nil, http.StatusNotFound)
 		return
 	}
 	_, errr := author.Delete(db.Connection)
 	if errr != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		response.Message = fmt.Sprintf("Erro : %s", errr.Error())
-		response.Code = http.StatusBadRequest
-		json.NewEncoder(w).Encode(response)
+		message := err.Error()
+		services.JSONError(w, message, nil, http.StatusNotFound)
 		return
 	}
-	w.WriteHeader(http.StatusOK)
-	response.Message = fmt.Sprintf("%s removido com successo", author.Name)
-	response.Code = http.StatusOK
-	json.NewEncoder(w).Encode(response)
+	message := "author removed successfully"
+	services.JSONSuccess(w, message, nil, 200)
 }
--- services/utils.go
+++ services/utils.go
@@ -1,9 +1,37 @@
 package services
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type HttpResponse struct {
-	Code    int              `json:"code"`
-	Message string           `json:"message"`
-	Data    *json.RawMessage `json:"data"`
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func JSONSuccess(w http.ResponseWriter, message string, data interface{}, code int) {
+	w.Header().Set("Content-Type", "application/json")
+	var response HttpResponse
+	if code == 0 {
+		response.Code = 200
+	}
+	response.Code = code
+	response.Message = message
+	response.Data = data
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(response)
+}
+func JSONError(w http.ResponseWriter, message string, data interface{}, code int) {
+	w.Header().Set("Content-Type", "application/json")
+	var response HttpResponse
+	if code == 0 {
+		response.Code = 501
+	}
+	response.Code = code
+	response.Message = message
+	response.Data = data
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(response)
 }
